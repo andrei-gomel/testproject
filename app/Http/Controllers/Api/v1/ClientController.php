d8f6948eeb7c95043bc930765c5883c0cc829c69
Андрей <84657319+andrei-gomel@users.noreply.github.com>
2025-10-31 14:43:21 +0300

    Update ClientController.php

--- app/Http/Controllers/Api/v1/ClientController.php
+++ app/Http/Controllers/Api/v1/ClientController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ClientUpdateRequest;
+use App\Http\Requests\ClientStoreRequest;
 use App\Models\Client;
 use Illuminate\Http\Request;
 
@@ -28,7 +29,7 @@ class ClientController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ClientStoreRequest $request)
     {
         $data = $request->all();
 
